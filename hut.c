b1478f28a4760381cadebe5d3280323c544e1b57
sameernagpal1391 <47549409+sameernagpal1391@users.noreply.github.com>
2019-09-04 22:19:29 +0530

    all codes with ss

--- hut.c
+++ hut.c
@@ -1,16 +1,16 @@
 #include<GL/glut.h>
 void myinit(void)
 {
- glClearColor(1.0,1.0,1.0,0.0);/*specifies red,green,blue and alpha (opacity) values used by glclear to clear the color buffers*/
+ glClearColor(1.0,1.0,1.0,0.0);
  glMatrixMode(GL_PROJECTION);
  glLoadIdentity();
- glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);/* indicates the clipping value for left, right , bottom, top, nearest and farthest value*/
+ glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
  }
  
  void hut(void)
  {
-glClear(GL_COLOR_BUFFER_BIT);/*performs clear operation on one or more buffers at the same time*/
-glColor3f(0.1,0.2,0.3); /* indicates the color for red, green and blue*/
+glClear(GL_COLOR_BUFFER_BIT);
+glColor3f(0.1,0.2,0.3);
 //rectangle
 	glBegin(GL_POLYGON);  
 	glVertex2f (0.1, 0.1);
@@ -50,23 +50,23 @@ glColor3f(0.2,1.0,0.2);
         glVertex2f (0.25, 0.35);
         glVertex2f (0.15, 0.35);
     glEnd();
-glColor3f(0.22,0.12,0.14);
+/*glColor3f(0.22,0.12,0.14);
 	glBegin(GL_POLYGON);
 	glVertex2f (0.6, 0.25);
         glVertex2f (0.7, 0.3);
         glVertex2f (0.6, 0.25);
         glVertex2f (0.7, 0.25);
-    glEnd();
+    glEnd();*/
 glFlush();
 }
 int main(int argc,char** argv)
 {
-glutInit(&argc,argv);/*initialize the toolkit*/
-glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);/*set the display mode*/
-glutInitWindowPosition(50,100);//set the window position on the screen
-glutInitWindowSize(640,480);//set the window size
-glutCreateWindow("hut");//open window with a title
+glutInit(&argc,argv);
+glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+glutInitWindowPosition(50,100);
+glutInitWindowSize(640,480);
+glutCreateWindow("hut");
 myinit();
-glutDisplayFunc(hut);//screen window is redrawn
+glutDisplayFunc(hut);
 glutMainLoop();
 }
